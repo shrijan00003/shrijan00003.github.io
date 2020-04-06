a0275591951840746a8eedbb458e839cf2270c68
shrijan <shrijan00003@gmail.com>
2020-04-06 22:03:34 +0545

    - sass added - main.yml fixed

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,10 +5,12 @@ import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
+import "../sass/app.scss"
+
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
+    <h1 className="primary-color">Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
